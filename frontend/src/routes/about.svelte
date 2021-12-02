62d41d159bf78ca59a52837206b95fa9370d9fef
AirisRX <airidasrupsas123@gmail.com>
2021-12-02 17:50:00 +0200

    Padarytas about us

---
+++ frontend/src/routes/about.svelte
@@ -0,0 +1,18 @@
+<main>
+    <h1 class="about">Saityno projektas</h1>
+    <p class="about">Šis projektas yra kūriamas Airido Rupšo ir Nerijaus Pocevičiaus. Daroma kompiuterių technikos parduotuvė.</p>
+    <p class="about">Kūriamas Klaipėdos Universiteto JNII21 Saityno technologijų dalykui.</p>
+    <p class="about">Naudojama:</p>
+    <p class="about">HTML - Svelte</p>
+    <p class="about">CSS - Bulma</p>
+    <p class="about">Duomenų bazė - Python, SQL, Docker</p>
+</main>
+
+<style>
+.about {
+    color: white;
+    margin: 20px auto;
+    width: 70%;
+    height: 50%;
+}
+</style>
