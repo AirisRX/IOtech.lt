ee85810f4eb2b188fbc7b0a393035f92c17826f9
AirisRX <airidasrupsas123@gmail.com>
2021-12-02 17:25:25 +0200

    Aptvarkytas auth puslapis

--- frontend/src/routes/auth.svelte
+++ frontend/src/routes/auth.svelte
@@ -1,5 +1,13 @@
 <script>
     import Auth from "$lib/auth.svelte"
 </script>
+<hr>
+<div class="center-align"><Auth/></div>
 
-<Auth/>
+<style>
+.center-align {
+  margin: 20px auto;
+  width: 70%;
+  height: 50%;
+}
+</style>
