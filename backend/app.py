14f9eb92b891ee751eb58b99106f7d92849f72f3
Nerijus Pocevičius <nerpoc42@gmail.com>
2021-12-11 00:18:41 +0200

    Paleisti naujai sukurti API, nuotraukos perkeltos į reikiamą trumpalaikę vietą

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,26 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from flask_restful import Api
 
 from api.auth import Email, Register, Login
 
-from api.product import CategoryAPI
+from api.product import CategoryAPI, ProductsAPI
 
 from database import db
 from settings import database_uri
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = database_uri 
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 db.init_app(app)
+
+
+@app.route('/img/<path:path>')
+def serve_static(path):
+    return send_from_directory('img', path)
+
 
 with app.app_context():
     db.create_all()
@@ -25,3 +31,4 @@ api.add_resource(Register, '/auth/register')
 api.add_resource(Login, '/auth/login')
 api.add_resource(Email, '/auth/email')
 api.add_resource(CategoryAPI, '/product/category')
+api.add_resource(ProductsAPI, '/product/products')
