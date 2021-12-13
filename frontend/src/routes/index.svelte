e3d42c45d5e835981466e1f1058de90e8e005cf8
AirisRX <airidas.rupsas@ku.lt>
2021-12-13 16:53:38 +0200

    Perrašytas index į bulma stilių, rodomi produktai

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -2,111 +2,105 @@
 	<title>IOTech.lt - kompiuterių technikos parduotuvė</title>
 </svelte:head>
 
+<script>
+    import 'bulma/css/bulma.min.css'
+
+    let produktai = []
+
+    async function getProducts() {
+        try {
+            const res = await fetch('http://localhost:5000/product/products?slug=nesiojami_kompiuteriai');
+            const text = await res.text()
+            // console.log(text)
+            produktai = JSON.parse(text)
+            console.log(produktai)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    getProducts();
+</script>
 
 <main>
-    <div id="center-align">
-        <hr>
-        <h1>Nauji produktai</h1>
-        <hr>
-        <a href="mailto:airidas.rupsas@ku.lt"> <img src="tikrainetas.jpg" alt="Reklama" title="Paspauskite, norint susisiekti"> </a>
-        <hr>
-        <h1>Nuolaidos, išpardavimai</h1>
-        <hr>
-        <h1>Rekomenduojami</h1>
-        <div class="items">
-                <div class="item"> 
-                    <img src="/products/dell-latitude-7410.jpg" alt="Dell kompiuteris">
-                    <a href="/">Dell</a>
-                    <p class="kaina">2000 euru</p>
-                    <button>Į krepšelį</button>
-                </div>
 
-                <div class="item">
-                    <a href="/">Lenovo</a> 
-                    <!-- Padaryti veikiančius linkus -->
-                </div>
+<section class="section level is-small">
+  <h1 class="title level-item">Nauji produktai</h1>
 
-                <div class="item">
-                    <a href="/">HP</a>
-                </div>
+  {#each produktai as produktas}
+    <div class="tile is-parent">
+        <article class="tile is-child notification is-info ">
+          <p class="title">{produktas["brand"]} {produktas["model"]}</p>
+          <p class="subtitle">{produktas["cost"]}€</p>
+          <figure class="image">
+            <img src="{produktas["img"]}" alt="Produktas">
+          </figure>
+        </article>
+      </div>
+  {/each}
 
-                <div class="item">
-                    <a href="/">Razer</a>
-                </div>
+</section>
 
-                <div class="item">
-                    <a href="/">Asus</a>
-                </div>
-			</div>
-			
+<hr>
+
+<a href="mailto:airidas.rupsas@ku.lt">
+    <div class="level">
+        <div class="level-item">
+            <img src="tikrainetas.jpg" alt="Reklama" title="Paspauskite, norint susisiekti" />
+        </div>
     </div>
-    
+</a>
+
+<hr>
+
+<section class="section level is-small">
+  <h1 class="title level-item">Išpardavimai</h1>
+
+  {#each produktai as produktas}
+  <div class="tile is horizontal">
+    <div class="tile is-parent">
+        <article class="tile is-child notification is-info ">
+          <p class="title">{produktas["brand"]} {produktas["model"]}</p>
+          <p class="subtitle">{produktas["cost"]}€</p>
+          <figure class="image">
+            <img src="{produktas["img"]}" alt="Produktas">
+          </figure>
+        </article>
+      </div>
+    </div>
+  {/each}
+
+</section>
+
+<hr>
+
+<section class="section level is-small">
+  <h1 class="title level-item">Rekomenduojami</h1>
+
+  {#each produktai as produktas}
+    <div class="tile is-parent">
+        <article class="tile is-child notification is-info ">
+          <p class="title">{produktas["brand"]} {produktas["model"]}</p>
+          <p class="subtitle">{produktas["cost"]}€</p>
+          <figure class="image">
+            <img src="{produktas["img"]}" alt="Produktas">
+          </figure>
+        </article>
+      </div>
+  {/each}
+
+</section>
+
+<hr>
+
 </main>
 
-<style lang="css">
+<style>
 
-
-
-.items {
-    display: flex;
-    align-content: space-between;
-    justify-content: space-evenly;
-    flex-wrap: nowrap;
-    margin: 25px;
-}
-
-.item {
-    width: fit-content;
-    gap: 0px;
-    border-radius: 5px;
-}
-
-.item a {
-    display: block;
-    text-align: center;
-    color: whitesmoke;
-}
-
-.item img {
-    width:auto;
-    height: 200px;
-    border-radius: 5px;
-}
-
-.kaina {
+.title
+{
     color: white;
+    display: flex;
 }
 
-#center-align {
-  margin: 20px auto;
-  width: 70%;
-  height: 50%;
-}
-
-
-
-
-
-
-
-
-
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		font-weight: 100;
-        text-align: left;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
