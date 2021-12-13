b0d88418d51b36a1c2ff2461f1f3138e47129b66
AirisRX <airidas.rupsas@ku.lt>
2021-12-13 16:52:54 +0200

    Perkeltas background į layout

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -6,16 +6,28 @@
 <main>
     <Nav/>
     
-    <nav class="breadcrumb is-right" aria-label="breadcrumbs">
+    <!-- <nav class="breadcrumb is-right" aria-label="breadcrumbs">
         <ul>
           <li><a href="/">IOtech</a></li>
           <li class="is-active"><a href="/" aria-current="page"></a>Kategorija</li>
         </ul>
-    </nav>
+    </nav> -->
 
     <slot></slot>
     <Footer/>
 </main>
 
 <style>
+:global(html), :global(body) {
+	position: relative;
+	width: 100%;
+	height: 100%;
+}
+
+:global(html) {
+    background-image: url("/background.jpg");
+    background-attachment: fixed;
+    font-family: 'Ubuntu', sans-serif;
+    background-size: auto;
+}
 </style>
