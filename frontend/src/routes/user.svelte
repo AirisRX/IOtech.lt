d191a6862419d1ca89a92d6c203621ff0c4aa0d5
AirisRX <airidas.rupsas@ku.lt>
2021-12-14 11:52:38 +0200

    Sukurtas vartotojo maketas

---
+++ frontend/src/routes/user.svelte
@@ -0,0 +1,54 @@
+<main>
+    <div class="container">
+    <div class="box" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 10px; margin-top: 15px;">
+
+        <section class="section">
+            <h1 class="title">Profilis</h1>
+            <div class="columns">
+                <div class="column">
+                    <div class="card">
+                        <div class="card-content">
+                          <div class="media">
+                            <div class="media-left">
+                              <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                              </figure>
+                            </div>
+                            <div class="media-content">
+                              <p class="title is-4 user">Vartotojas</p>
+                              <p class="subtitle is-6">Paštas</p>
+                            </div>
+                          </div>
+                      
+                          <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                            <a href="#">#css</a> <a href="#">#responsive</a>
+                            <br>
+                            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                          </div>
+                        </div>
+                      </div>
+                </div>
+                <div class="column">
+                  Second column
+              </div>
+        </section>
+</div>
+</div>
+
+</main>
+
+<style>
+
+.title
+{
+    color: white;
+}
+
+.user
+{
+    color: grey;
+}
+
+</style>
