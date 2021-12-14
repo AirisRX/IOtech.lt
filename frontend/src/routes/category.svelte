2794bc2af7de1bb2c8f33a5c848de36b71fd98f5
AirisRX <airidas.rupsas@ku.lt>
2021-12-14 11:30:18 +0200

    Padarytas pradinis kategorijų puslapio maketas

--- frontend/src/routes/category.svelte
+++ frontend/src/routes/category.svelte
@@ -1,28 +1,104 @@
-<!-- Kiekvienas kategorijos pasirinkimas nukreipiamas į šį puslapį, padaryti, kad generuotu vartotojo pasirinkimą automatiškai.
-Apačioje maketas. -->
-
 <script>
-    import 'bulma/css/bulma.css'
+	import 'bulma/css/bulma.css';
+	import 'bulma/css/bulma.min.css';
+
+	let produktai = [];
+
+	async function getProducts() {
+		try {
+			const res = await fetch('http://localhost:5000/product/products?slug=nesiojami_kompiuteriai');
+			const text = await res.text();
+			// console.log(text)
+			produktai = JSON.parse(text);
+			console.log(produktai);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	getProducts();
+
+	let notification;
+
+	function toggleNotification(text) {
+		console.log('veikia ar neveikia stai kur klausimas');
+		if (notification) {
+			notification = null;
+			return;
+		}
+		notification = text;
+		setInterval(() => {
+			notification = null;
+		}, 3000);
+	}
 </script>
 
 <main>
-    <h1>Insert category selection here</h1>
-    
-    <aside class="menu">
-        <p class="menu-label">
-          Filtras
-        </p>
-        <ul class="menu-list">
-          <li><a>Pasirinkimas</a></li>
-        </ul>
-      </aside>
+	<div class="container">
+		{#if notification}
+			<div>
+				<div class="notification has-background-white has-text-primary has-text-weight-bold">
+					<p>{notification}</p>
+				</div>
+			</div>
+		{/if}
+	</div>
 
+  <div class="box container" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 10px; margin-top: 15px;">
+    <section class="section">
+      <h1 class="title">Filtras</h1>
+      <div class="columns">
+        <div class="column">
+          Prekių kaina:
+        </div>
+        <div class="column">
+          Gamintojas:
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <div class="container">
+  <div class="box" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 10px; margin-top: 15px;">
+    <section class="section level is-small">
+      <div class="tile is-ancestor">
+        <div class="tile">
+          {#each produktai as produktas}
+            <div class="tile is-parent is-3">
+              <article class="tile is-child notification is-info">
+                <p class="title">{produktas['brand']} {produktas['model']}</p>
+                <figure class="image">
+                  <img src={produktas['img']} alt="Produktas" style="border-radius: 5px;" />
+                  <div class="columns">
+                    <div class="column">
+                      <p class="subtitle">{produktas['cost']}€</p>
+                    </div>
+                    <button class="button is-primary is-rounded">
+                      <span class="icon is-left"><i class="fas fa-shopping-bag" /></span>
+                      <span on:click={() => toggleNotification("Jūsų prekė buvo pridėta į krepšelį.")}>Į krepšelį</span>
+                    </button>
+                  </div>
+                </figure>
+              </article>
+            </div>
+          {/each}
+        </div>
+      </div>
+    </section>
+    </div>
+    
+
+  </div>
 </main>
 
 <style>
-
-h1 {
+  .title
+  {
+color: white;
+padding-bottom: 30px;
+  }
+  .columns
+  {
     color: white;
-}
-
+  }
 </style>
