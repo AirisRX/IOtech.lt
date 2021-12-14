26386cb0cd10fbbdd16e480a736822b37feb02be
AirisRX <airidasrupsas123@gmail.com>
2021-12-14 20:03:44 +0200

    Padarytas pradinis paieškos maketas

---
+++ frontend/src/routes/search.svelte
@@ -0,0 +1,12 @@
+<main>
+    <div class="box container" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 10px; margin-top: 15px;">
+        <h1 class="title">Paieška</h1>
+    </div>
+</main>
+
+<style>
+    .title
+    {
+        color: white;
+    }
+</style>
