adaace20160c4d1e80822d7113098f5c1d8173bd
Nerijus Pocevičius <nerpoc42@gmail.com>
2021-12-15 23:20:11 +0200

    Atnaujintas kategorijų API, pridėtas slugas

--- backend/api/product/category.py
+++ backend/api/product/category.py
@@ -1,27 +1,23 @@
-from collections import defaultdict
-
 from flask_restful import Resource
 from models.category import Category
 from database import db
-from sqlalchemy.sql import functions as func
-from pprint import pprint
 
 
 class CategoryAPI(Resource):
     def get(self):
 
-        sub_q1 = db.session.query(Category.id, Category.name).filter_by(parent_id=None).cte(recursive=True, name='s')
+        sub_q1 = db.session.query(Category.id, Category.name, Category.slug).filter_by(parent_id=None).cte(recursive=True, name='s')
 
         name_column = (sub_q1.c.name + db.literal("|") + Category.name)
 
-        sub_q2 = db.session.query(Category.id, name_column).join(
+        sub_q2 = db.session.query(Category.id, name_column, Category.slug).join(
             sub_q1, Category.parent_id == sub_q1.c.id)
 
         sub_q = sub_q1.union_all(
             sub_q2
         )
 
-        categories = db.session.query(sub_q.c.name.label('name')).all()
+        categories = db.session.query(sub_q.c.name.label('name'), sub_q.c.slug.label('slug')).all()
 
         formatted_c = {}
 
@@ -32,7 +28,7 @@ class CategoryAPI(Resource):
             for n in names[:-1]:
                 ref = ref[n]["kategorijos"]
 
-            ref[names[-1]] = {"kategorijos": {}}
+            ref[names[-1]] = {"kategorijos": {}, "slug": c.slug}
             # if c.description:
             #     ref[names[-1]]["aprasas"] = c.description
 
