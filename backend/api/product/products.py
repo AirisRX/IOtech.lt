798300b0a2b30e82ea6b8e95d5080dc5d2a99d17
Nerijus Pocevičius <nerpoc42@gmail.com>
2021-12-16 01:16:06 +0200

    Atnaujintas produktų API, pridėtas pasirinktini parametrai slugas ir pavadinimas

--- backend/api/product/products.py
+++ backend/api/product/products.py
@@ -13,12 +13,18 @@ from database import db
 class ProductsAPI(Resource):
     def get(self):
         slug = request.args.get('slug')
+        name = request.args.get('name')
+
         q = db.session.query(Comp.id, Brand.name.label("brand"), CompSI.model, Product.cost,
-                             Product.count, CompIMG.url.label("img")).select_from(Category).filter(Category.slug == slug)
+                             Product.count, CompIMG.url.label("img")).select_from(Category)
+        if slug:
+            q = q.filter(Category.slug == slug)
         q = q.join(CompSI, Category.atid == CompSI.atid)
         q = q.join(Comp, Comp.csid == CompSI.id)
         q = q.join(Product, Product.cmid == Comp.id)
         q = q.join(Brand, Brand.id == CompSI.bid, isouter=True)
+        if name:
+            q = q.filter((Brand.name + db.literal(" ") + CompSI.model).contains(name))
         q = q.join(CompIMG, CompIMG.id == Comp.thumbnail_id, isouter=True)
 
         res = q.all()
