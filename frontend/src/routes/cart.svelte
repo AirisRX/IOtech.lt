ecc7c31526c7723cc470e54b9cfda54304959a5d
AirisRX <airidasrupsas123@gmail.com>
2021-12-16 05:04:59 +0200

    Pridėtas pradinis krepšelio maketas

--- frontend/src/routes/cart.svelte
+++ frontend/src/routes/cart.svelte
@@ -1,1 +1,12 @@
+<main>
+    <div class="box container" style="background-color: rgba(0, 0, 0, 0.6); border-radius: 10px; margin-top: 15px;">
+        <h1 class="title">Krepšelis</h1>
+    </div>
+</main>
 
+<style>
+    .title
+    {
+        color: white;
+    }
+</style>
